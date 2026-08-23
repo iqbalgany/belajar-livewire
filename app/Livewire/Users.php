83471985b9a6836056f82d322d62448fc7caa620
iqbalgany <rismagany1203@gmail.com>
2026-08-23 14:21:03 +0700

    feat: add action button for crate new user

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,12 +3,22 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
 
     public $title = 'Users Page';
+
+    public function createUser()
+    {
+        User::create([
+            'name' => 'Iqbal',
+            'email' => 'iqbal@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
 
     public function render()
     {
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,9 +1,17 @@
-<div>
-    <h1 class="text-3xl font-bold">{{ $title }}</h1>
+<div class="w-1/2 m-auto my-10">
+    <h1 class="text-3xl font-semibold">{{ $title }}</h1>
 
     <p>Users count: {{ count($users) }}</p>
 
-    @foreach ($users as $user)
-        <div>{{ $user->name }}</div>
-    @endforeach
+
+    <button wire:click="createUser" type="button"
+        class="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 my-2 cursor-pointer">Create
+        User</button>
+
+    <hr class="text-2xl font-bold mb-2">User List</h2>
+    <ul class="list-disc">
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>

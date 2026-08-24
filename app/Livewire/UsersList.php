add4add714bb309938c45be2c8b959a8041517bf
iqbalgany <rismagany1203@gmail.com>
2026-08-24 23:26:08 +0700

    feat: add polling and lazy loading

--- app/Livewire/UsersList.php
+++ app/Livewire/UsersList.php
@@ -25,8 +25,16 @@ class UsersList extends Component
         $this->resetPage();
     }
 
+    public function placeholder()
+    {
+        return view('livewire.placeholders.skeleton');
+    }
+
     public function render()
     {
+
+        sleep(1);
+
         return view('livewire.users-list', [
             'title' => 'Users Page',
             'users' => User::latest()
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -16,7 +16,7 @@
     <div class="flex justify-center gap-10 px-5">
 
         @livewire('users-register-form')
-        @livewire('users-list')
+        @livewire('users-list', ['lazy' => true])
     </div>
 
 
